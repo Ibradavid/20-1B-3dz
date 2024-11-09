2438fae56331722fa53565ea8e7b98e2114d4828
Ibradavid <Ibraimovdavid0@gmail.com>
2024-11-09 23:13:23 +0600

    commit

--- apps/library/serializers.py
+++ apps/library/serializers.py
@@ -4,4 +4,4 @@ from apps.library.models import Library
 class LibrarySerializer(serializers.ModelSerializer):
     class Meta:
         model = Library
-        fields = ['id','title','author']
+        fields = ['id','title','author','created_at',]
